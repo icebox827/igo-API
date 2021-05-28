0058068a28509114f100979dd8efdfc1589677a5
Denis Lafontant <icebox827@gmail.com>
2021-05-28 18:15:20 -0400

    Create association for booked_car

--- app/models/booked_car.rb
+++ app/models/booked_car.rb
@@ -1,2 +1,4 @@
 class BookedCar < ApplicationRecord
+  belongs_to :user
+  belongs_to :car
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ApplicationRecord
   validates :make, :model, :year, :color, :transmission, :seats, :image_url, presence: true
 
   belongs_to :user
-  has_many :booked_cars
+  has_one :booked_cars
 end
