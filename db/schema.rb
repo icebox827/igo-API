278ea9f1767d942d873f585d229450c90c5c34fa
Denis Lafontant <icebox827@gmail.com>
2021-05-28 16:44:14 -0400

    Reset db

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,4 @@ ActiveRecord::Schema.define(version: 2021_05_28_200128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cars", force: :cascade do |t|
-    t.string "make"
-    t.string "model"
-    t.integer "year"
-    t.string "color"
-    t.string "transmission"
-    t.integer "seats"
-    t.string "image_url"
-    t.string "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_cars_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "email"
-    t.boolean "admin", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    username { "MyString" }
-    password { "MyString" }
-    email { "MyString" }
-    admin { false }
-  end
-end
