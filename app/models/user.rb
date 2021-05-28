eb4655647eddf782e2d5505a2d3700bd5c5c1073
Denis Lafontant <icebox827@gmail.com>
2021-05-28 18:05:03 -0400

    Add validation and association tu user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { in: 3..20 }
+  validates :password, presence: true
+
+  has_many :cars
 end
