45e1f0cebd1808cc4b29da85b332165b83d020c6
Denis Lafontant <icebox827@gmail.com>
2021-05-28 18:08:10 -0400

    Update association and validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { in: 3..20 }
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
 
   has_many :cars
+  has_many :booked_cars
 end
