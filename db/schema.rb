4f834b925f0a8ce0ebf289aa86f4cbf44d2bf234
Denis Lafontant <icebox827@gmail.com>
2021-05-28 17:03:12 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_205531) do
+ActiveRecord::Schema.define(version: 2021_05_28_210205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.integer "year"
+    t.string "color"
+    t.string "transmission"
+    t.string "image_url"
+    t.integer "seats"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
