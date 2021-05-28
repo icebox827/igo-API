2a1c74a208782ff6a40c441a065776cccff1a387
Denis Lafontant <icebox827@gmail.com>
2021-05-28 18:48:09 -0400

    Update users controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,27 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      
+      def index
+        @users = User.all
+
+        render json: UsersRepresenter.new(users).as_json
+      end
+
+      def create
+        @user = User.create(user_params)
+
+        if @user.save
+          render json: UsersRepresenter.new(users).as_json, status: :created
+        else
+          render json: { error: 'Not created' }, status: :Not_processed
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
     end
   end
 end
