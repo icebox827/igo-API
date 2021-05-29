6f7955fc2819302a1730279b76cc886757dc68d8
Denis Lafontant <icebox827@gmail.com>
2021-05-29 16:12:55 -0400

    Update routes with root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
   namespace :api do
     namespace :v1, default: { format: :json } do
       resources :users, only: %i[index show create]
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
       resources :sessions, only: %i[create destroy]
     end
   end
+
+  get '*path', to: 'pages#index', via: :all
+
   scope :sessions do
     get 'sign_out' => 'sessions#destroy'
   end
