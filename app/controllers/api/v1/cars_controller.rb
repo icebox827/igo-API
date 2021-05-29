d709e6869d7b2f05594794ed99d6cb7a14cb21a1
Denis Lafontant <icebox827@gmail.com>
2021-05-29 10:14:10 -0400

    Update car controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,11 +1,16 @@
 module Api
   module V1
     class CarsController < ApplicationController
-
       def index
         @cars = Car.all
 
         render json: @cars
+      end
+
+      private
+
+      def car_params
+        params.require(:car).permit(:make, :model, :year, :color, :transmission, :seats, :image_url)
       end
     end
   end
