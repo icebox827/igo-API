ec9c224eefb1bd2d23e6531501032de2fb834ac5
Denis Lafontant <icebox827@gmail.com>
2021-05-29 15:55:14 -0400

    Add new field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_215746) do
+ActiveRecord::Schema.define(version: 2021_05_29_195006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_05_28_215746) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "cars", "users"
