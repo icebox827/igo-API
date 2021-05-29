34de97db98c12d52de1c599f54dd99721d8a7413
Denis Lafontant <icebox827@gmail.com>
2021-05-29 16:23:59 -0400

    Update user controller with serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,29 +2,29 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        @users = User.all
-        render json: @users
+        users = User.all
+        render json: UsersSerializer.new(users).serialized_json
       end
 
       def show
-        @user = User.find(params[:id])
-        render json: @user
+        user = User.find(params[:id])
+        render json: UsersSerializer.new(user).serialized_json
       end
 
       def create
-        @user = User.create(user_params)
+        user = User.new(user_params)
 
         if @user.save
-          render json: @user, status: :created
+          render json: UsersSerializer.new(user).serialized_json
         else
-          render json: { message: @user.error.full_message }, status: :Not_processed
+          render json: { error: user.errors.messages }, status: 422
         end
       end
 
       private
 
       def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :email, :password)
       end
     end
   end
