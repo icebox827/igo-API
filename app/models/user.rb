94753e76950f4c5cfb2270aafa287e5f2a34e692
Denis Lafontant <icebox827@gmail.com>
2021-05-29 16:02:30 -0400

    Add new validation to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { in: 3..20 }
   validates :password, presence: true, length: { minimum: 8 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: /\A\S+@.+\.\S+\z/
 
-  has_many :cars
+  has_many :cars, dependent: :destroy
   has_many :booked_cars, dependent: :destroy
 end
