79aab0ef844279d63ad0031c71a82ac11607df02
Denis Lafontant <icebox827@gmail.com>
2021-05-29 15:07:25 -0400

    Creaet routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, default: { format: :json } do
+      resources :users, only: [:index, :show, :create]
+      resources :cars
+      resources :booked_cars
+    end
+  end
 end
