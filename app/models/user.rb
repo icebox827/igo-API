974256ccf07894700775a9ff84ce9b67a45215d8
Denis Lafontant <icebox827@gmail.com>
2021-05-29 10:49:43 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { in: 3..20 }
   validates :password, presence: true, length: { minimum: 8 }
 
   has_many :cars
-  has_many :booked_cars
+  has_many :booked_cars, dependent: :destroy
 end
