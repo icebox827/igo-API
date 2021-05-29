680305e165b0d150b7c43217a7c2c4bc56852654
Denis Lafontant <icebox827@gmail.com>
2021-05-29 15:17:19 -0400

    Format routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1, default: { format: :json } do
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: %i[index show create]
       resources :cars
       resources :booked_cars
-      resources :sessions, only: [:create, :destroy]
+      resources :sessions, only: %i[create destroy]
     end
   end
-
-  delete 'api/v1/sessions/:id', to: 'api/v1/sessions#destroy'
+  scope :sessions do
+    get 'sign_out' => 'sessions#destroy'
+  end
 end
