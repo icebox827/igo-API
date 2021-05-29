739f5f094ace00a953fb78e837a50ce1e1a48196
Denis Lafontant <icebox827@gmail.com>
2021-05-29 13:50:03 -0400

    Update car controller with all the methods

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -3,8 +3,47 @@ module Api
     class CarsController < ApplicationController
       def index
         @cars = Car.all
-
         render json: @cars
+      end
+
+      def show
+        @car = Car.find(params[:id])
+        render json: @car
+      end
+
+      def new
+        @car = Car.new(car_params)
+        render json: @car
+      end
+
+      def create
+        @car = Car.create(car_params)
+
+        if @car.save
+          render json: @car, status: :created
+        else
+          render json: { message: @user.error.full_message }, status: :Not_processed
+        end
+      end
+
+      def update
+        @car = Car.find(params[:id])
+
+        if @car.update
+          render json: @car, status: :updated
+        else
+          render json: { message: @user.error.full_message }, status: :Not_processed
+        end
+      end
+
+      def destroy
+        @car = Car.find(params[:id])
+
+        if @car.destry
+          render json: @car, status: :destroyed
+        else
+          render json: { message: @user.error.full_message }, status: :Not_processed
+        end
       end
 
       private
