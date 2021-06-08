3511849c06e3347324cfe571391c876145a7f38a
Denis Lafontant <icebox827@gmail.com>
2021-06-08 18:37:48 -0400

    Hide secret key with Rails.secrets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::API
 
     if authorization_header
       token = authorization_header.split[1]
-      secret_key = '5496fs6a6rgra6g1'
+      secret_key = Rails.application.secrets.secret_key_base[0]
+      # secret_key = '5496fs6a6rgra6g1'
       decoded_token = JWT.decode(token, secret_key)
 
       @user = User.find(decoded_token[0]['user_id'])
