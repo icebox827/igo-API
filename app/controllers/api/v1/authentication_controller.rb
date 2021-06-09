d43b7984802c377468480571a80c3870533d971e
Denis Lafontant <icebox827@gmail.com>
2021-06-09 11:05:01 -0400

    Update status code

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,14 +4,14 @@ module Api
       def create
         @user = User.find_by(username: params[:username])
         if !@user
-          render json: { error: 'Invalid username' }
+          render json: { error: 'Invalid username' }, status: 403
         elsif @user.authenticate(params[:password])
           secret_key = ENV['SECRET_KEY']
           token = JWT.encode({ user_id: @user.id, username: @user.username }, secret_key)
 
           render json: { token: token, admin: @user.admin }, status: :ok
         else
-          render json: { error: 'Invalid password' }
+          render json: { error: 'Invalid password' }, status: 403
         end
       end
     end
